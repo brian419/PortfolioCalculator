e633ecc29d1b781dac733623e70a33d6090ba9e6
brian419 <json10@crimson.ua.edu>
2022-11-06 12:15:17 -0600

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Brian Son on 11/6/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
